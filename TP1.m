907ace7a85a94ec66a85d4c80a0512fc0b6e632e
lbntc <83251173+lbntc@users.noreply.github.com>
2022-11-05 16:23:07 +0100

    Update TP1.m

--- TP1.m
+++ TP1.m
@@ -2,15 +2,21 @@ clear all, close all, clc;
 n=25; k=5; 
 Texte=input('message','s');
 Choix=input('entrez zero pour chiffrer, un pour dechiffrer');
+
 T=dec2bin(abs(Texte),8);
 %chiffrement
+if(Choix==0)
 for i=1:size(T,1)
     S(i,:)=RF(T(i,:),n,k,0);
+    
 end
 crypto=char(bin2dec(S))
+
     %dechiffrement
-    G=dec2bin(abs(crypto),8);
+else
+    G=dec2bin(abs(Texte),8);
     for i=1:size(G,1)
         S(i,:)=RF(G(i,:),n,k,1);
+        end
+  clair=char(bin2dec(S))
     end
-    clair=char(bin2dec(S))
